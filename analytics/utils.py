f0471b75bedbb9536bbbf9a53b7cb2d60aa6557c
xslates <wilfried.nguessan@outlook.com>
2019-10-30 14:00:45 +0100

    added analytics management

--- analytics/utils.py
+++ analytics/utils.py
@@ -12,5 +12,10 @@ import re
 def clean_names(dataset: pd.DataFrame) -> pd.DataFrame:
     dataset.columns = [re.sub("(.)([A-Z][a-z]+)", r"\1_\2", w).lower() for w in dataset.columns]
     dataset.columns = [re.sub(":", "_", w) for w in dataset.columns]
-    dataset.columns = [re.sub("\.", "_", w) for w in dataset.columns]
+    dataset.columns = [re.sub(r"\.", "_", w) for w in dataset.columns]
     return dataset
+
+
+def explode_columns():
+    # want to explode columns with lists of dictionaries (t.ex user permissions in GTM has containerAccess column)
+    pass
