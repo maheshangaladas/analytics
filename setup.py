9ebdc8b8c710805c3f10516934e194b5090c39d0
xslates <wilfried.nguessan@outlook.com>
2019-10-31 18:21:37 +0100

    updated release in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="analytics",
-    version="0.0.2",
+    version="0.0.3",
     author="Wilfried N'Guessan",
     author_email="wilfried.nguessan@outlook.com",
     description="Web analytics tooling",
