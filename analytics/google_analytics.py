7dd2fa193b276b3c76d35bf5ad9449836e0813df
xslates <wilfried.nguessan@outlook.com>
2019-10-31 18:17:14 +0100

    decorators to streamline flatting of JSON responses

--- analytics/google_analytics.py
+++ analytics/google_analytics.py
@@ -1,5 +1,5 @@
 """
-google.google_analytics
+analytics.google_analytics
 .......................
 
 functions to query the google analytics API
@@ -13,95 +13,141 @@ ref. https://code.markedmondson.me/anti-sampling-google-analytics-api/
 ref. https://developers.google.com/analytics/devguides/config/mgmt/v3/unsampled-reports
 """
 
-import json
-import re
-from typing import List, Optional
-
 import pandas as pd
-from pandas.io.json import json_normalize
 
-from analytics.utils import clean_names
+from analytics.utils import flatten
 
 
+@flatten
 def ga_list_account_summaries(service) -> pd.DataFrame:
-    account_summaries = service.management().accountSummaries().list().execute()
-    account_summaries = json_normalize(account_summaries, record_path=["items", "webProperties", "profiles"])
-    account_summaries = clean_names(account_summaries)
+    account_summaries = (
+        service.management().accountSummaries().list().execute()["items"]
+    )
     return account_summaries
 
 
+@flatten
 def ga_list_accounts(service) -> pd.DataFrame:
-    accounts = service.management().accounts().list().execute()
-    accounts = json_normalize(accounts, record_path="items")
-    accounts = clean_names(accounts)
+    accounts = service.management().accounts().list().execute()["items"]
     return accounts
 
 
+@flatten
 def ga_list_webproperties(service, account_id: str) -> pd.DataFrame:
-    properties = service.management().webproperties().list(accountId=account_id).execute()
-    properties = json_normalize(properties, record_path="items")
-    properties = clean_names(properties)
+    properties = (
+        service.management()
+        .webproperties()
+        .list(accountId=account_id)
+        .execute()["items"]
+    )
     return properties
 
 
+@flatten
 def ga_list_account_users(service, account_path: str) -> pd.DataFrame:
-    account_users = service.management().accountUserLinks().list(accountId=account_path).execute()
-    account_users = json_normalize(account_users, record_path="items")
-    account_users = clean_names(account_users)
+    account_users = (
+        service.management()
+        .accountUserLinks()
+        .list(accountId=account_path)
+        .execute()["items"]
+    )
     return account_users
 
 
-def ga_adwords_links(service, account_id: str, webproperty_id: str) -> pd.DataFrame:
-    adwords_links = service.management().webPropertyAdWordsLinks().list(accountId=account_id, webPropertyId=webproperty_id).execute()
-    adwords_links = json_normalize(adwords_links, record_path=["items", "adWordsAccounts"])
-    adwords_links = clean_names(adwords_links)
+@flatten
+def ga_adwords_links(
+    service, account_id: str, webproperty_id: str
+) -> pd.DataFrame:
+    adwords_links = (
+        service.management()
+        .webPropertyAdWordsLinks()
+        .list(accountId=account_id, webPropertyId=webproperty_id)
+        .execute()["items"]
+    )
     return adwords_links
 
 
-def ga_list_custom_dimensions(service, account_id: str, webproperty_id: str) -> pd.DataFrame:
-    custom_dimensions = service.management().customDimensions().list(accountId=account_id, webPropertyId=webproperty_id).execute()
-    custom_dimensions = json_normalize(custom_dimensions, record_path="items")
-    custom_dimensions = clean_names(custom_dimensions)
+@flatten
+def ga_list_custom_dimensions(
+    service, account_id: str, webproperty_id: str
+) -> pd.DataFrame:
+    custom_dimensions = (
+        service.management()
+        .customDimensions()
+        .list(accountId=account_id, webPropertyId=webproperty_id)
+        .execute()["items"]
+    )
     return custom_dimensions
 
 
-def ga_list_custom_metrics(service, account_id: str, webproperty_id: str) -> pd.DataFrame:
-    custom_metrics = service.management().customMetrics().list(accountId=account_id, webPropertyId=webproperty_id).execute()
-    custom_metrics = json_normalize(custom_metrics, record_path="items")
-    custom_metrics = clean_names(custom_metrics)
+@flatten
+def ga_list_custom_metrics(
+    service, account_id: str, webproperty_id: str
+) -> pd.DataFrame:
+    custom_metrics = (
+        service.management()
+        .customMetrics()
+        .list(accountId=account_id, webPropertyId=webproperty_id)
+        .execute()["items"]
+    )
     return custom_metrics
 
 
+@flatten
 def ga_list_filters(service, account_id: str) -> pd.DataFrame:
-    filters = service.management().filters().list(accountId=account_id).execute()
-    filters = json_normalize(filters, record_path="items")
-    filters = clean_names(filters)
+    filters = (
+        service.management()
+        .filters()
+        .list(accountId=account_id)
+        .execute()["items"]
+    )
     return filters
 
 
-def ga_list_views(service, account_id: str, webproperty_id: str) -> pd.DataFrame:
-    views = service.management().profiles().list(accountId=account_id, webPropertyId=webproperty_id).execute()
-    views = json_normalize(views, record_path="items")
-    views = clean_names(views)
+@flatten
+def ga_list_views(
+    service, account_id: str, webproperty_id: str
+) -> pd.DataFrame:
+    views = (
+        service.management()
+        .profiles()
+        .list(accountId=account_id, webPropertyId=webproperty_id)
+        .execute()["items"]
+    )
     return views
 
 
-def ga_list_goals(service, account_id: str, webproperty_id: str, view_id: str) -> pd.DataFrame:
-    goals = service.management().goals().list(accountId=account_id, webPropertyId=webproperty_id, profileId=view_id).execute()
-    goals = json_normalize(goals, record_path="items")
-    goals = clean_names(goals)
+@flatten
+def ga_list_goals(
+    service, account_id: str, webproperty_id: str, view_id: str
+) -> pd.DataFrame:
+    goals = (
+        service.management()
+        .goals()
+        .list(
+            accountId=account_id,
+            webPropertyId=webproperty_id,
+            profileId=view_id,
+        )
+        .execute()["items"]
+    )
     return goals
 
 
-def ga_list_remarketing_audiences(service, account_id: str, webproperty_id: str) -> pd.DataFrame:
-    remarketing_audiences = service.management().remarketingAudience().list(accountId=account_id, webPropertyId=webproperty_id).execute()
-    remarketing_audiences = json_normalize(remarketing_audiences, record_path="items")
-    remarketing_audiences = clean_names(remarketing_audiences)
+@flatten
+def ga_list_remarketing_audiences(
+    service, account_id: str, webproperty_id: str
+) -> pd.DataFrame:
+    remarketing_audiences = (
+        service.management()
+        .remarketingAudience()
+        .list(accountId=account_id, webPropertyId=webproperty_id)
+        .execute()["items"]
+    )
     return remarketing_audiences
 
 
+@flatten
 def ga_list_segments(service) -> pd.DataFrame:
-    segments = service.management().segments().list().execute()
-    segments = json_normalize(segments, record_path="items")
-    segments = clean_names(segments)
+    segments = service.management().segments().list().execute()["items"]
     return segments
--- analytics/google_tag_manager.py
+++ analytics/google_tag_manager.py
@@ -8,62 +8,96 @@ ref. https://developers.google.com/tag-manager/api/v2/devguide
 """
 
 import pandas as pd
-from pandas.io.json import json_normalize
 
-from analytics.utils import clean_names
+from analytics.utils import flatten
 
 
+@flatten
 def gtm_list_accounts(service) -> pd.DataFrame:
-    accounts = service.accounts().list().execute()
-    accounts = json_normalize(accounts, record_path="account")
-    accounts = clean_names(accounts)
+    accounts = service.accounts().list().execute()["account"]
     return accounts
 
 
+@flatten
 def gtm_list_permissions(service, account_path: str) -> pd.DataFrame:
-    permissions = service.accounts().user_permissions().list(parent=account_path).execute()
-    permissions = json_normalize(permissions, record_path="userPermission")
-    permissions = clean_names(permissions)
+    permissions = (
+        service.accounts()
+        .user_permissions()
+        .list(parent=account_path)
+        .execute()["userPermission"]
+    )
     return permissions
 
 
+@flatten
 def gtm_list_containers(service, account_path: str) -> pd.DataFrame:
-    containers = service.accounts().containers().list(parent=account_path).execute()
-    containers = json_normalize(containers, record_path="container")
-    containers = clean_names(containers)
+    containers = (
+        service.accounts()
+        .containers()
+        .list(parent=account_path)
+        .execute()["container"]
+    )
     return containers
 
 
+@flatten
 def gtm_list_workspaces(service, container_path: str) -> pd.DataFrame:
-    workspaces = service.accounts().containers().workspaces().list(parent=container_path).execute()
-    workspaces = json_normalize(workspaces, record_path="workspace")
-    workspaces = clean_names(workspaces)
+    workspaces = (
+        service.accounts()
+        .containers()
+        .workspaces()
+        .list(parent=container_path)
+        .execute()["workspace"]
+    )
     return workspaces
 
 
+@flatten
 def gtm_list_environments(service, container_path: str) -> pd.DataFrame:
-    environments = service.accounts().containers().environments().list(parent=container_path).execute()
-    environments = json_normalize(environments, record_path="environment")
-    environments = clean_names(environments)
+    environments = (
+        service.accounts()
+        .containers()
+        .environments()
+        .list(parent=container_path)
+        .execute()["environment"]
+    )
     return environments
 
 
-def gtm_list_variables(service, workspace_path: str) -> pd.DataFrame:
-    variables = service.accounts().containers().workspaces().variables().list(parent=workspace_path).execute()
-    variables = json_normalize(variables, record_path="variable")
-    variables = clean_names(variables)
-    return variables
-
-
+@flatten
 def gtm_list_tags(service, workspace_path: str) -> pd.DataFrame:
-    tags = service.accounts().containers().workspaces().tags().list(parent=workspace_path).execute()
-    tags = json_normalize(tags, record_path="tag")
-    tags = clean_names(tags)
+    tags = (
+        service.accounts()
+        .containers()
+        .workspaces()
+        .tags()
+        .list(parent=workspace_path)
+        .execute()["tag"]
+    )
     return tags
 
 
+@flatten
+def gtm_list_variables(service, workspace_path: str) -> pd.DataFrame:
+    variables = (
+        service.accounts()
+        .containers()
+        .workspaces()
+        .variables()
+        .list(parent=workspace_path)
+        .execute()["variable"]
+    )
+    return variables
+
+
+@flatten
 def gtm_list_triggers(service, workspace_path: str) -> pd.DataFrame:
-    triggers = service.accounts().containers().workspaces().triggers().list(parent=workspace_path).execute()
-    triggers = json_normalize(triggers, record_path="trigger")
-    triggers = clean_names(triggers)
+    triggers = (
+        service.accounts()
+        .containers()
+        .workspaces()
+        .triggers()
+        .list(parent=workspace_path)
+        .execute()["trigger"]
+    )
     return triggers
