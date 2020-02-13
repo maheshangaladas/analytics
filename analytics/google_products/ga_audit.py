1ff79ef1e791b1a0726e2ec908dcc46dc5b8b973
xslates <wilfried.nguessan@outlook.com>
2020-02-13 16:58:03 +0100

    ga audit revamp, f*cking galant work

--- analytics/google_products/ga_audit.py
+++ analytics/google_products/ga_audit.py
@@ -1,11 +1,12 @@
-from collections import OrderedDict
 from pprint import pprint
-from typing import Dict, List
 
 import googleapiclient
 
 from analytics.google_products.authorization import Service
 from analytics.google_products.google_analytics import GAUser
+
+# from typing import Dict, List
+
 
 # no automatic checks instead:
 # - create summaries
@@ -20,75 +21,109 @@ def ga_auth(client: str) -> googleapiclient.discovery.Resource:
     return ga_user
 
 
-def ga_accounts_audit(
-    user: googleapiclient.discovery.Resource, account_ids: List[str]
-) -> Dict:
-    """retrieve overview for a list of google analytics accounts"""
+def account_collection(user, account_ids):
+    summaries = user.summaries()["items"]
+    selected_accounts = [d for d in summaries if d["id"] in account_ids]
+    ids = [d["id"] for d in selected_accounts]
+    names = [d["name"] for d in selected_accounts]
+    return list(zip(ids, names))
+
+
+def filter_collection(user, account_ids):
+    filters = [user.filters(i)["items"] for i in account_ids]
+    ids = [i["accountId"] for f in filters for i in f]
+    names = [i["name"] for f in filters for i in f]
+    return list(zip(ids, names))
+
+
+def property_collection(user, account_ids):
+    properties = [user.properties(i)["items"] for i in account_ids]
+    ids = [i["id"] for p in properties for i in p]
+    names = [i["name"] for p in properties for i in p]
+    return list(zip(ids, names))
+
+
+def profile_collection(user, account_ids):
     summaries = user.summaries()
     items = summaries["items"]
-    accounts = [d for d in items if d["id"] in account_ids]
-    account_names = [d["name"] for d in accounts]
-    properties_names = [
-        j["name"] for i in accounts for j in i["webProperties"]
+    selected_accounts = [d for d in items if d["id"] in account_ids]
+
+    ids = [
+        j["id"]
+        for i in selected_accounts
+        for j in i["webProperties"]
+        for k in j["profiles"]
     ]
-    properties_ids = [j["id"] for i in accounts for j in i["webProperties"]]
-    n_properties = len(properties_names)
-    profile_names = [
+
+    names = [
         k["name"]
-        for i in accounts
+        for i in selected_accounts
         for j in i["webProperties"]
         for k in j["profiles"]
     ]
-    profile_ids = [
-        k["id"]
-        for i in accounts
-        for j in i["webProperties"]
-        for k in j["profiles"]
+
+    return list(zip(ids, names))
+
+
+def permission_collection(user, account_ids):
+    perms = [user.users(i) for i in account_ids]
+    users = [i["userRef"]["email"] for p in perms for i in p["items"]]
+    access = [i["permissions"]["effective"] for p in perms for i in p["items"]]
+    return list(zip(users, access))
+
+
+def adwords_collection(user, account_ids):
+    ids = [j["id"] for i in account_ids for j in user.properties(i)["items"]]
+
+    # (we're getting the item list but we can add another nesting too)
+
+    adwords = [
+        user.adwords_links(j["accountId"], j["id"])["items"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
     ]
-    n_profiles = len(profile_names)
 
-    overview: Dict = OrderedDict(
-        [
-            ("account_names", account_names),
-            ("properties_name", properties_names),
-            ("properties_tally", n_properties),
-            ("properties_ids", properties_ids),
-            ("profile_names", profile_names),
-            ("profiles_tally", n_profiles),
-            ("profile_ids", profile_ids),
-        ]
-    )
-
-    return overview
+    return list(zip(ids, adwords))
 
 
-def ga_permissions_audit(
-    user: googleapiclient.discovery.Resource, account_ids: List[str]
-) -> Dict:
-    """retrieve permissions overview for a list of google analytics accounts"""
-    permissions = [user.users(i) for i in account_ids]
-    effective_permissions = [
-        j["permissions"]["effective"] for i in permissions for j in i["items"]
+def custdim_collection(user, account_ids):
+    ids = [
+        j["id"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.custom_dimensions(j["accountId"], j["id"])["items"]
     ]
-    user_reference = [
-        j["userRef"]["email"] for i in permissions for j in i["items"]
+
+    names = [
+        k["name"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.custom_dimensions(j["accountId"], j["id"])["items"]
     ]
-    overview: Dict = OrderedDict(zip(user_reference, effective_permissions))
-    return overview
+
+    return list(zip(ids, names))
 
 
-def ga_properties_audit(user: googleapiclient.discovery.Resource):
-    summaries = user.summaries()["items"]
-    # we need to create a dictionary of account_id, property_id
-    # we then can decompose it in the call to funcs like custom_metrics
-    return summaries
+def custmet_collection(user, account_ids):
+    ids = [
+        j["id"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.custom_metrics(j["accountId"], j["id"])["items"]
+    ]
+
+    names = [
+        k["name"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.custom_metrics(j["accountId"], j["id"])["items"]
+    ]
+
+    return list(zip(ids, names))
 
 
-def ga_profiles_audit(user: googleapiclient.discovery.Resource):
-    summaries = user.summaries()["items"]
-    # we need a dict of account_id, property_id, view_id
-    # we then decompose it to make call to funcs like goals
-    return summaries
+def goals_collection(user, accout_ids):
+    pass
 
 
 if __name__ == "__main__":
@@ -103,9 +138,12 @@ if __name__ == "__main__":
 
     gcp_client = "gcp-client.json"
     ga_user = ga_auth(gcp_client)
-    accounts = ga_accounts_audit(ga_user, nvv_accounts)
-    permissions = ga_permissions_audit(ga_user, nvv_accounts)
-    # pprint(accounts)
-    # pprint(permissions)
-    properties = ga_properties_audit(ga_user)
-    pprint(properties)
+
+    # pprint(account_collection(ga_user, nvv_accounts))
+    # pprint(filter_collection(ga_user, nvv_accounts))
+    # pprint(property_collection(ga_user, nvv_accounts))
+    # pprint(profile_collection(ga_user, nvv_accounts))
+    # pprint(permission_collection(ga_user, nvv_accounts))
+    # pprint(adwords_collection(ga_user, nvv_accounts))
+    # pprint(custdim_collection(ga_user, nvv_accounts))
+    pprint(custmet_collection(ga_user, nvv_accounts))
