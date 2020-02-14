dc2f9159ee38270810d38ee72dbd1fbe3f498af7
xslates <wilfried.nguessan@outlook.com>
2020-02-14 11:24:09 +0100

    ga audit v.1 :)

--- analytics/google_products/ga_audit.py
+++ analytics/google_products/ga_audit.py
@@ -5,23 +5,16 @@ import googleapiclient
 from analytics.google_products.authorization import Service
 from analytics.google_products.google_analytics import GAUser
 
-# from typing import Dict, List
-
-
-# no automatic checks instead:
-# - create summaries
-# - write summaries to file
-# - inspect and evaluate files manually
-
 
 def ga_auth(client: str) -> googleapiclient.discovery.Resource:
-    """authorize into google analytics API"""
+    """authorize into the google analytics API"""
     service = Service("analytics", "v3", client).authenticate()
     ga_user = GAUser(service)
     return ga_user
 
 
-def account_collection(user, account_ids):
+def accounts_collection(user, account_ids):
+    """return (account id, name) for selected accounts"""
     summaries = user.summaries()["items"]
     selected_accounts = [d for d in summaries if d["id"] in account_ids]
     ids = [d["id"] for d in selected_accounts]
@@ -29,27 +22,30 @@ def account_collection(user, account_ids):
     return list(zip(ids, names))
 
 
-def filter_collection(user, account_ids):
+def filters_collection(user, account_ids):
+    """return (account id, filter name) for selected accounts"""
     filters = [user.filters(i)["items"] for i in account_ids]
     ids = [i["accountId"] for f in filters for i in f]
     names = [i["name"] for f in filters for i in f]
     return list(zip(ids, names))
 
 
-def property_collection(user, account_ids):
+def properties_collection(user, account_ids):
+    """return (property id, property name) for selected accounts"""
     properties = [user.properties(i)["items"] for i in account_ids]
     ids = [i["id"] for p in properties for i in p]
     names = [i["name"] for p in properties for i in p]
     return list(zip(ids, names))
 
 
-def profile_collection(user, account_ids):
+def profiles_collection(user, account_ids):
+    """return (property name, profile name) for selected accounts"""
     summaries = user.summaries()
     items = summaries["items"]
     selected_accounts = [d for d in items if d["id"] in account_ids]
 
-    ids = [
-        j["id"]
+    properties = [
+        j["name"]
         for i in selected_accounts
         for j in i["webProperties"]
         for k in j["profiles"]
@@ -62,10 +58,11 @@ def profile_collection(user, account_ids):
         for k in j["profiles"]
     ]
 
-    return list(zip(ids, names))
+    return list(zip(properties, names))
 
 
-def permission_collection(user, account_ids):
+def permissions_collection(user, account_ids):
+    """return (email, permissions) for selected accounts"""
     perms = [user.users(i) for i in account_ids]
     users = [i["userRef"]["email"] for p in perms for i in p["items"]]
     access = [i["permissions"]["effective"] for p in perms for i in p["items"]]
@@ -73,9 +70,11 @@ def permission_collection(user, account_ids):
 
 
 def adwords_collection(user, account_ids):
-    ids = [j["id"] for i in account_ids for j in user.properties(i)["items"]]
+    """return (property name, adwords ids / or names (??)) for selected accounts"""
+    ids = [j["name"] for i in account_ids for j in user.properties(i)["items"]]
 
     # (we're getting the item list but we can add another nesting too)
+    # (this is not finished yet)
 
     adwords = [
         user.adwords_links(j["accountId"], j["id"])["items"]
@@ -86,9 +85,10 @@ def adwords_collection(user, account_ids):
     return list(zip(ids, adwords))
 
 
-def custdim_collection(user, account_ids):
+def custdims_collection(user, account_ids):
+    """return (property name, custom dimensions) for selected accounts"""
     ids = [
-        j["id"]
+        j["name"]
         for i in account_ids
         for j in user.properties(i)["items"]
         for k in user.custom_dimensions(j["accountId"], j["id"])["items"]
@@ -104,9 +104,10 @@ def custdim_collection(user, account_ids):
     return list(zip(ids, names))
 
 
-def custmet_collection(user, account_ids):
+def custmets_collection(user, account_ids):
+    """return (property name, custom metrics) for selected accounts"""
     ids = [
-        j["id"]
+        j["name"]
         for i in account_ids
         for j in user.properties(i)["items"]
         for k in user.custom_metrics(j["accountId"], j["id"])["items"]
@@ -122,8 +123,29 @@ def custmet_collection(user, account_ids):
     return list(zip(ids, names))
 
 
-def goals_collection(user, accout_ids):
-    pass
+def goals_collection(user, account_ids):
+    """return (property name, goals) for selected account"""
+    properties = [
+        j["name"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.views(j["accountId"], j["id"])["items"]
+        for g in user.goals(k["accountId"], k["webPropertyId"], k["id"])[
+            "items"
+        ]
+    ]
+
+    names = [
+        g["name"]
+        for i in account_ids
+        for j in user.properties(i)["items"]
+        for k in user.views(j["accountId"], j["id"])["items"]
+        for g in user.goals(k["accountId"], k["webPropertyId"], k["id"])[
+            "items"
+        ]
+    ]
+
+    return list(zip(properties, names))
 
 
 if __name__ == "__main__":
@@ -139,11 +161,12 @@ if __name__ == "__main__":
     gcp_client = "gcp-client.json"
     ga_user = ga_auth(gcp_client)
 
-    # pprint(account_collection(ga_user, nvv_accounts))
-    # pprint(filter_collection(ga_user, nvv_accounts))
-    # pprint(property_collection(ga_user, nvv_accounts))
-    # pprint(profile_collection(ga_user, nvv_accounts))
-    # pprint(permission_collection(ga_user, nvv_accounts))
-    # pprint(adwords_collection(ga_user, nvv_accounts))
-    # pprint(custdim_collection(ga_user, nvv_accounts))
-    pprint(custmet_collection(ga_user, nvv_accounts))
+    pprint(accounts_collection(ga_user, nvv_accounts))
+    pprint(filters_collection(ga_user, nvv_accounts))
+    pprint(properties_collection(ga_user, nvv_accounts))
+    pprint(profiles_collection(ga_user, nvv_accounts))
+    pprint(permissions_collection(ga_user, nvv_accounts))
+    pprint(adwords_collection(ga_user, nvv_accounts))
+    pprint(custdims_collection(ga_user, nvv_accounts))
+    pprint(custmets_collection(ga_user, nvv_accounts))
+    pprint(goals_collection(ga_user, nvv_accounts))
